4c5e2fb99b8d46c9949175c992af7d14496ebf2f
Simon Ndiritu <deshtey@gmail.com>
2024-01-28 02:11:49 +0300

    student subject

---
+++ schoolapp/schoolapp.domain/Entities/Academics/StudentSubject.cs
@@ -0,0 +1,8 @@
+﻿namespace schoolapp.Domain.Entities.Academics
+{
+    internal class StudentSubject
+    {
+        public int StudentId { get; set; }
+        public int SubjectId { get; set; }
+    }
+}
